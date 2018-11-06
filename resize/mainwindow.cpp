3969a22d5661c024dd63616a03bd5d7cfca9b65d
mbjapzon <mbjapzon@gmail.com>
2018-11-06 16:20:35 -0500

    Add files via upload

--- resize/mainwindow.cpp
+++ resize/mainwindow.cpp
@@ -3,6 +3,8 @@
 
 #include <QGraphicsScene>
 
+static int count = 0;
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
@@ -22,5 +24,32 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    //TODO resize the test widgets
+
+    int i = 0;
+
+    foreach(TestWidget * test, m_sequence->getChildrenTests())
+    {
+        //Increase the first TestWidget by a height of 100
+        if(i ==0)
+        {
+            QRectF testGeometry = test->geometry();
+            testGeometry.setHeight(testGeometry.height() + 100);
+            test->setGeometry(testGeometry);
+        }
+        else
+        {
+            //Adjust the y coordinate of the subsequence tests by 100
+            QRectF testGeometry = test->geometry();
+            testGeometry.adjust(0, 100, 0, 0);
+            test->setGeometry(testGeometry);
+        }
+        i++;
+
+    }
+
+    //Increase the height of the sequence by 100
+    QRectF sequenceGeometry = m_sequence->geometry();
+    sequenceGeometry.setHeight(sequenceGeometry.height() + 100);
+    m_sequence->setGeometry(sequenceGeometry);
+
 }
